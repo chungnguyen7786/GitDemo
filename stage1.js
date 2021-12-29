45c75dea92ed5576d39b39015215bef20a7db589
Chung Nguyen <themask7786@gmail.com>
2021-12-29 12:25:00 +0700

    stage 4 commit

--- stage1.js
+++ stage1.js
@@ -1,3 +1,4 @@
 console.log("This file was create at stage 1");
 console.log("This is changed at stage 2");
 console.log("This is changed at stage 3");
+console.log("This is changed at stage 4");
--- stage3.js
+++ stage3.js
@@ -1,2 +1,2 @@
 console.log("This file was create at stage 3");
-
+console.log("This is changed at stage 4");
