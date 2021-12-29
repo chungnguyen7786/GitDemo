81b2b981388cfbb2ab411eae29731a4530265dca
Chung Nguyen <71998866+chungnguyen7786@users.noreply.github.com>
2021-12-29 17:14:36 +0700

    Update stage1.js from branch2

--- stage1.js
+++ stage1.js
@@ -1,5 +1,6 @@
-console.log("This file was modified at branch1");
+console.log("This file was modified at branch2");
 console.log("This is changed at stage 2");
 console.log("This is changed at stage 3");
 console.log("This is changed at stage 4");
 console.log("This is created at branch1");
+console.log("This is created at branch2");
